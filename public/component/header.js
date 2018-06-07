4b0da73677c1413cac12952e0c126f610ffdf4ae
mxAbdiwali <AM2850@live.mdx.ac.uk>
2018-06-08 00:09:46 +0100

    added navbar

--- public/component/header.js
+++ public/component/header.js
@@ -5,7 +5,7 @@ class Navbar extends React.Component{
      render(){
          return(
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand">{console.log(this.state)}</a>
+                <a className="navbar-brand">{}</a>
                 <form className="form-inline">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
